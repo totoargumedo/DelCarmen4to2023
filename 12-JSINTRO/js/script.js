2c2bb9634e9f8d0f898ceebbff4cfe90f71e2229
Toto Argumedo <totoargumedo@gmail.com>
2023-08-16 13:28:18 -0300

    JS intro

---
+++ 12-JSINTRO/js/script.js
@@ -0,0 +1,22 @@
+// console.log("Hola no soy mi primer console");
+// console.warn("Hola soy otro console");
+// console.error("Hola soy un error");
+// console.log(3);
+// console.log(true);
+// console.log(false);
+// console.log(null);
+// console.log(undefined);
+
+// alert("alerta");
+// alert(3);
+// alert(true);
+
+// prompt("Ingrese el nombre");
+
+// let nombre = prompt("Ingrese el nombre");
+
+// console.log(nombre);
+
+let edad = prompt("Ingrese su edad");
+
+console.log(edad);
