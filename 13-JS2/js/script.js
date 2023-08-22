05a518ee5a97e1ebc7dbab8e63b7b927996fe166
Toto Argumedo <totoargumedo@gmail.com>
2023-08-22 13:19:33 -0300

    Variables en JS

---
+++ 13-JS2/js/script.js
@@ -0,0 +1,44 @@
+// let comida = "Milanesas";
+// const comida2 = "Pizza";
+
+// console.log(comida);
+// console.log(comida2);
+
+// comida = 233;
+
+// console.log(comida);
+// console.log(comida2);
+// console.log(typeof comida);
+// console.log(typeof comida2);
+
+// let data;
+
+// console.log(data);
+
+// data = prompt("Dame toda la data");
+
+// console.log(data);
+
+// var
+
+// let number1 = Number(prompt("Ingrese numero 1"));
+// let number2 = Number(prompt("Ingrese numero 2"));
+// let suma = number1 + number2;
+// let resta = number1 - number2;
+// let division = number1 / number2;
+// let multiplicacion = number1 * number2;
+// let resto = number1 % number2;
+// let potencia = number1 ** number2;
+
+// console.log("La suma de los numeros ingresados es: " + suma);
+// console.log("La resta de los numeros ingresados es: " + resta);
+// console.log("La division de los numeros ingresados es: " + division);
+// console.log(
+//   "La multiplicacion de los numeros ingresados es: " + multiplicacion
+// );
+// console.log("La resto de los numeros ingresados es: " + resto);
+// console.log("La potencia de los numeros ingresados es: " + potencia);
+
+let name = prompt("Ingrese nombre");
+
+console.log("Bienvenido/a " + name);
